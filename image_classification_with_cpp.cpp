c69577fb4d8208e01e9c519efff3d04488421072
atifkarim <atif0902082@gmail.com>
2019-01-11 23:50:18 +0100

    initialize cpp file to convert python/keras classify code into cpp

---
+++ image_classification_with_cpp.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include<cstring>
+#include<string>
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <vector>
+#include <assert.h>
+#include <cmath>
+#include<sstream>
+#include <fstream>
