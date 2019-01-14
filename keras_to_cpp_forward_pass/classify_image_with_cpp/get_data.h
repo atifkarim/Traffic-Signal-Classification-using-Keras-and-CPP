cac47d51ad5f8cc8c8f0c21e6d8353f0d8d7b87b
atifkarim <atif0902082@gmail.com>
2019-01-14 11:10:24 +0100

    project file for image classification with cpp using Keras trained model

---
+++ keras_to_cpp_forward_pass/classify_image_with_cpp/get_data.h
@@ -0,0 +1,21 @@
+#ifndef GET_DATA_H
+#define GET_DATA_H
+
+
+class Fetch_Data
+{
+
+public:
+
+//    bool getFileContent(std::string fileName, std::vector<std::string> & vecOfStrs);
+    Image convolution_kernal ();
+    Matrix conv_bias_value ();
+    Matrix dense_value ();
+    Matrix dense_bias_value ();
+
+
+};
+
+
+
+#endif // GET_DATA_H
